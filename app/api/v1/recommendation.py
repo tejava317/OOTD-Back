198786ff73c1c0dea12c10753fe2dfd1ecf5c3ec
tejava317 <frogjj@kaist.ac.kr>
2025-01-08 20:56:11 +0900

    fix: Fix tiny problem

--- app/api/v1/recommendation.py
+++ app/api/v1/recommendation.py
@@ -34,24 +34,24 @@ async def generate_recommendation(request: RecommendationRequest):
             "겉옷: [두꺼운 패딩, 울 코트, 경량 패딩, 가벼운 재킷, 겉옷 없음]\n"
             "상의: [두꺼운 니트, 긴팔 티셔츠, 반팔 티셔츠]\n"
             "하의: [기모 바지, 긴 바지, 반바지]\n"
-            "신발 및 양말: [부츠와 두꺼운 양말, 운동화와 얇은 양말, 샌들/슬리퍼]\n"
+            "신발: [부츠, 운동화, 샌들/슬리퍼]\n"
             "Answer in Korean while adhering to the following json format.\n"
             "겉옷: \n"
             "상의: \n"
             "하의: \n"
-            "신발 및 양말: \n"
+            "신발: \n"
         )
         response = gemini.get_response(prompt)
         
         response = response.replace('```json', '').replace('```', '').strip()
         parsed_response = json.loads(response)
-        
+
         return RecommendationResponse(
             message="Outfit recommendation successfully returned.",
             outer=parsed_response["겉옷"],
             top=parsed_response["상의"],
             bottom=parsed_response["하의"],
-            shoes=parsed_response["신발 및 양말"]
+            shoes=parsed_response["신발"]
         )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
